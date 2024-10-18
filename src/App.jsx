b6ed7aad75efa85b080cdda68797c444088b1fe8
Mirkomedia <mirko.weibel18@gmail.com>
2024-10-18 12:46:24 +0300

    almost at the finish line

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,25 @@
-import { useState, useEffect } from 'react'
 import './App.css'
 import { CountryDetails } from './components/CountryDetails'
-
+import { useState, useEffect } from 'react';
 
 function App() {
+ 
+  const [error, setError] = useState(null);    
   const [country, setCountry] = useState({});
   const [loading, setLoading] = useState(true);
-
-
- const [countryData, setCountryData] = useState({
-    cca3: "mon"
+  const [countries, setCountries] = useState({})
+   const [countryData, setCountryData] = useState({
+    cca3: "RUS"
   })
   const  countryCode = countryData.cca3
-  useEffect(() =>{
+ useEffect(() =>{
     const fetchCountry = async () =>{
       setLoading(true);
       try {
         const response = await fetch(`https://restcountries.com/v3.1/alpha?codes=${countryCode}`);
         const data = await response.json();
-        console.log(data)
         setCountry(data[0])
-        setLoading(false);
-        console.log(country.name.common)
+        setLoading(false)
       } catch (error) {
         console.log('Error fetching data', error)
         setLoading(false);
@@ -29,13 +27,31 @@ function App() {
  
     };
    fetchCountry();
-  }, [] ); 
-   if(loading){
-    return( 
-      <p>Loading...</p>
-    )
-   }
- 
+  }, [countryCode] ); 
+
+useEffect(() =>{
+   const fetchCountries = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`https://restcountries.com/v3.1/all`);
+      const data = await response.json();
+      setCountries(data);
+    } catch (error) {
+      setError('Failed to fetch country data. Please try again.');
+    } finally {
+      setLoading(false);
+    }
+  };
+  fetchCountries();
+}, [] );
+
+if(loading){
+  return( 
+    <p>Loading...</p>
+  )
+ }
+
+
   return (
     <>
       <div>
@@ -45,6 +61,11 @@ function App() {
       <CountryDetails 
       country={country}
       setCountry={setCountry}
+      countryCode={countryCode}
+      loading={loading}
+      setLoading={setLoading}
+      countries={countries}
+      
   />
    
     </>
--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -2,21 +2,31 @@ import React from 'react'
 import { useEffect, useState } from 'react'
 import './CountryDetails.css'
 
-export const CountryDetails = ({ country,setCountryData}) => {
-
-  console.log(country)
+export const CountryDetails = ({ country,setCountryData, countries}) => {
   const flagSrc = country.flags.png;
-
   const population = country.population;
   const formattedPop = population.toLocaleString();
   const nativeNames = country.name?. nativeName;
   const borders = country?.borders 
-  const commaBorders = borders ? borders.join(', ') : 'none'
   const languages = country.languages ? Object.values(country.languages).join(', ') : 'Penguin Speech'
 
-  console.log(country.cca3)
-  console.log(commaBorders)
-  const renderCurrencies = () => {
+
+  //convert the countries data into key-value pairs, where each pair is an array
+  const newMap = (Object.entries(countries));
+
+
+  const filterBorderCountries = newMap
+  //destructure each entry with the ([_,country]) where the _is used to ignore the key
+  //while country holds the individual country object.
+  //filter function checks if the cca3 code of the country is included in the borders 
+  //array using borders.includes(country.cca3). This returns an array that only contains
+  // countries whose cca3 codes are included in the borders array
+  //then I map the filtered array to return an array consisting of the countries common
+  //names
+  .filter(([_, country]) => borders.includes(country.cca3)) 
+  .map(([_, country]) => country.name.common); 
+
+    const renderCurrencies = () => {
     if(country.currencies)
     return Object.entries(country.currencies).map(([code, { name, symbol }]) => (
        
@@ -26,8 +36,17 @@ export const CountryDetails = ({ country,setCountryData}) => {
   const renderNativeNames = () =>{
     if(nativeNames)
     return Object.entries(nativeNames).map(([langCode, names]) => names.common).join(', ')}
-   
 
+  const renderBorders = () => {
+ 
+    if(borders)
+     return (filterBorderCountries).map(( borderCountry, index,) => (
+           
+      <span key={index} className='border-countries' >{borderCountry} </span>
+           
+        ));} 
+   
+        
   return (
     
     <div className='top-lvl-container'>
@@ -68,12 +87,11 @@ export const CountryDetails = ({ country,setCountryData}) => {
         <div className='border-countries-container'>
            
             <div className='border-countries-list'>
-            <p><b>Border Countries: </b><span className='border-countries'>{commaBorders}</span></p>
-            <p>{country.borders ? country.borders : ''}</p>
+            <p><b>Border Countries: </b><span >{country.borders ? renderBorders()  : 'None' }</span></p>
+           
             </div>
         </div>
       </div>
-
 
      </div>
     </div>
