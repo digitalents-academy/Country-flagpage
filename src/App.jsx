bdf2a5f9e2972adb00fe6844ff939d7adc27a663
Mirkomedia <mirko.weibel18@gmail.com>
2024-10-21 12:31:13 +0300

    addedSearch

--- src/App.jsx
+++ src/App.jsx
@@ -66,7 +66,7 @@ if(loading){
       </div>
 
       <div>
-        <SearchBar />
+      {state === 0 && <SearchBar setCountryData={setCountryData} nextState={nextState} />}
       </div>
 
       <div>
--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import { useRef } from "react"
 import "../catalogue.css"
 
-export default function SearchBar() {
+export default function SearchBar({ setCountryData, nextState }) {
+  const inputRef = useRef(null);
+  
+  function searchCountry(){
+    if(inputRef.current && inputRef.current.value !== ''){
+      setCountryData(inputRef.current.value)
+      nextState()
+    }
+   }
 
+   function handleKeyDown(event){
+    if(event.key === 'Enter' ) 
+    searchCountry();
+   }
   return (
     <div className="search-bars">
       <div className="search-bar">
         <img className="search-icon" src="src/search-outline.svg" />
-        <input className="search-bar-text" type="text" placeholder="Search for a country..."></input>
+        <input ref={inputRef} onKeyDown={handleKeyDown} className="search-bar-text" type="text" placeholder="Search for a country..."></input>
       </div>
 
       <form action="/action_page.php">
