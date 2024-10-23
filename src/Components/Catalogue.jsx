ff8939274831d1076b66c9edc48d78040ddaae1d
Pinja Ylitolonen <ylitpin@digitalentshelsinki.internal>
2024-10-23 13:50:38 +0300

    lol

--- src/Components/Catalogue.jsx
+++ src/Components/Catalogue.jsx
@@ -7,41 +7,41 @@ export default function Catalogue({ countriesData, setCountriesData, countryData
 
   const newMap = Object.entries(countriesData)
   //switch to using useRef isntead of queryselector
- 
-  // Function to set countryData and execute nextState
-/* function handleCountryClick(event) {
-  const clickedBox = event.currentTarget; // Get the clicked element
-  const countryValue = clickedBox.querySelector('p').innerText;
-  setCountryData(countryValue) // Update countryData object
-  nextState(); // Call nextState function
-} */
-function handleCountryClick(countryName){
-  if(!countryName){
-    console.error('country name is undefined')
-    return;
-  }
-  setCountryData(countryName)
-  nextState();
-}
-/* // Get all elements with the "country-box" class
-const countryBoxes = document.querySelectorAll('.country-box');
 
-// Add event listeners to each country box
-//breaks after re-rendering the component
-/* countryBoxes.forEach(box => {
-  box.addEventListener('click', handleCountryClick);
-});
- */ 
+  // Function to set countryData and execute nextState
+  /* function handleCountryClick(event) {
+    const clickedBox = event.currentTarget; // Get the clicked element
+    const countryValue = clickedBox.querySelector('p').innerText;
+    setCountryData(countryValue) // Update countryData object
+    nextState(); // Call nextState function
+  } */
+  function handleCountryClick(countryName) {
+    if (!countryName) {
+      console.error('country name is undefined')
+      return;
+    }
+    setCountryData(countryName)
+    nextState();
+  }
+  /* // Get all elements with the "country-box" class
+  const countryBoxes = document.querySelectorAll('.country-box');
+  
+  // Add event listeners to each country box
+  //breaks after re-rendering the component
+  /* countryBoxes.forEach(box => {
+    box.addEventListener('click', handleCountryClick);
+  });
+   */
   const displayCountries = () => {
     if (countriesData)
       return newMap.map(([key, { name, population, region, capital, flags }]) => (
-        <div key={key}  className="country-box" onClick={() => handleCountryClick(name.common)} >
+        <div key={key} className="country-box" onClick={() => handleCountryClick(name.common)} >
           <img className="flag" src={flags.png} />
           <div className="country-info">
             <p className="country">{name.common}</p>
             <p className="population"><b>Population:</b> {population.toLocaleString()}</p>
             <p className="region"><b>Region:</b> {region}</p>
-            <p className="capital"><b>Capital:</b> {capital}</p>
+            <p className="capital"><b>Capital:</b> {capital && Array.isArray(capital) ? capital.join(', ') : capital || 'none'}</p>
           </div>
         </div>
       ))
--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,20 +1,28 @@
-import { useRef } from "react"
+import { useRef, useState } from "react"
 import "../catalogue.css"
 
 export default function SearchBar({ setCountryData, nextState }) {
+  const [selectedValue, setSelectedValue] = useState('Option 1');
   const inputRef = useRef(null);
-  
-  function searchCountry(){
-    if(inputRef.current && inputRef.current.value !== ''){
+
+  function searchCountry() {
+    if (inputRef.current && inputRef.current.value !== '') {
       setCountryData(inputRef.current.value)
       nextState()
     }
-   }
+  }
 
-   function handleKeyDown(event){
-    if(event.key === 'Enter' ) 
-    searchCountry();
-   }
+  function handleKeyDown(event) {
+    if (event.key === 'Enter')
+      searchCountry();
+  }
+
+  const handleChange = (event) => {
+    setSelectedValue(event.target.value);
+    console.log(event.target.value)
+  };
+
+
   return (
     <div className="search-bars">
       <div className="search-bar">
@@ -22,20 +30,15 @@ export default function SearchBar({ setCountryData, nextState }) {
         <input ref={inputRef} onKeyDown={handleKeyDown} className="search-bar-text" type="text" placeholder="Search for a country..."></input>
       </div>
 
-      <form action="/action_page.php">
-        <select className="filter" defaultValue="filter" name="region" id="regions">
-          <option value="filter" disabled="true">Filter by Region</option>
-          <option value="africa">Africa</option>
-          <option value="america">America</option>
-          <option value="asia">Asia</option>
-          <option value="europe">Europe</option>
-          <option value="oceania">Oceania</option>
-        </select>
-      </form>
+      <select className="dropdown" value={selectedValue} onChange={handleChange}>
+        <option value="Filter by Region">Filter by Region</option>
+        <option value="Option 1">Africa</option>
+        <option value="Option 2">America</option>
+        <option value="Option 3">Europe</option>
+        <option value="Option 4">Oceania</option>
+      </select>
     </div>
 
 
   )
-
-
 }
